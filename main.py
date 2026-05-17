03883e180979fbfffcbbec156d4db4e42ac1123d
denis milyukov <denis.milyukov97@mail.ru>
2026-05-17 20:10:43 +0300

    fixing project structure

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ pd.set_option('display.width', 1000)
 res = pd.DataFrame({
 })
 
-print(os.listdir('csv'))
-df = pd.read_excel('csv/доля_зож.xls', header=1)
+#print(os.listdir('csv'))
+df = pd.read_excel('../data/raw/доля_зож.xls', header=1)
 print(df)
 
 res['region'] = df.iloc[:,0]
@@ -18,14 +18,14 @@ print(res)
 
 res['region'] = res['region'].str.strip()
 
-df = pd.read_excel('csv/доля_несовершеннолетних_участников_преступлений.xls', header=1)
+df = pd.read_excel('../data/raw/доля_несовершеннолетних_участников_преступлений.xls', header=1)
 df['mean'] = df.iloc[:, 1:].mean(axis=1)
 df[df.columns[0]] = df[df.columns[0]].str.strip()
 res = res.merge(df[[df.columns[0], 'mean']], how='left', left_on='region', right_on=df.columns[0]).rename(columns={'mean':'ratio_underage_partic'})
 res = res.drop(res.columns[len(res.columns)-2], axis = 1)
 print(res)
 
-df = pd.read_excel('csv/количество_дипломов_на100тыс_человек.xlsx', header=0)
+df = pd.read_excel('../data/raw/количество_дипломов_на100тыс_человек.xlsx', header=0)
 df['mean'] = df.iloc[:, 1:].mean(axis=1)
 print(df)
 
@@ -33,7 +33,7 @@ res = res.merge(df[[df.columns[0], 'mean']], how='left', left_on='region', right
 res = res.drop(res.columns[len(res.columns)-2], axis = 1)
 print(res)
 
-df = pd.read_excel('csv/коэф_джини.xls', header=2)
+df = pd.read_excel('../data/raw/коэф_джини.xls', header=2)
 df['mean'] = df.iloc[:, 1:].mean(axis=1)
 df[df.columns[0]] = df[df.columns[0]].str.strip()
 res = res.merge(df[[df.columns[0], 'mean']], how='left', left_on='region', right_on=df.columns[0]).rename(columns={'mean':'coeff_Gini'})
@@ -41,7 +41,7 @@ res = res.drop(res.columns[len(res.columns)-2], axis = 1)
 print(df)
 print(res)
 
-df = pd.read_excel('csv/Население,_не_употребляющее_табачные_и_нетабачные_курительные_и.xlsx', header=3)
+df = pd.read_excel('../data/raw/Население,_не_употребляющее_табачные_и_нетабачные_курительные_и.xlsx', header=3)
 print(df)
 df['mean_male'] = df.iloc[1:, 1::2].mean(axis=1)
 df['mean_female'] = df.iloc[1:, 2::2].mean(axis=1)
@@ -52,7 +52,7 @@ res = res.drop(res.columns[len(res.columns)-3], axis = 1)
 
 print(res)
 
-df = pd.read_excel('csv/преступление_лица.xls', header=1)
+df = pd.read_excel('../data/raw/преступление_лица.xls', header=1)
 df['mean'] = df.iloc[:, 1:].mean(axis=1)
 df[df.columns[0]] = df[df.columns[0]].str.strip()
 res = res.merge(df[[df.columns[0], 'mean']], how='left', left_on='region', right_on=df.columns[0]).rename(columns={'mean':'num_of_criminals'})
@@ -60,49 +60,49 @@ res = res.drop(res.columns[len(res.columns)-2], axis = 1)
 print(res)
 
 
-df = pd.read_excel('csv/преступления_алкогольное_опьянение.xls', header=1)
+df = pd.read_excel('../data/raw/преступления_алкогольное_опьянение.xls', header=1)
 df['mean'] = df.iloc[:, 1:].mean(axis=1)
 df[df.columns[0]] = df[df.columns[0]].str.strip()
 res = res.merge(df[[df.columns[0], 'mean']], how='left', left_on='region', right_on=df.columns[0]).rename(columns={'mean':'num_of_alcohol_crimes'})
 res = res.drop(res.columns[len(res.columns)-2], axis = 1)
 print(res)
 
-df = pd.read_excel('csv/преступления_женщины.xls', header=1)
+df = pd.read_excel('../data/raw/преступления_женщины.xls', header=1)
 df[df.columns[0]] = df[df.columns[0]].str.strip()
 res = res.merge(df[[df.columns[0], 'Среднее']], how='left', left_on='region', right_on=df.columns[0]).rename(columns={'Среднее':'num_of_female_crimes'})
 res = res.drop(res.columns[len(res.columns)-2], axis = 1)
 print(res)
 
-df = pd.read_excel('csv/преступления_нарко_опьянение.xls', header=1)
+df = pd.read_excel('../data/raw/преступления_нарко_опьянение.xls', header=1)
 df['mean'] = df.iloc[:, 1:].mean(axis=1)
 df[df.columns[0]] = df[df.columns[0]].str.strip()
 res = res.merge(df[[df.columns[0], 'mean']], how='left', left_on='region', right_on=df.columns[0]).rename(columns={'mean':'num_of_drugs_crimes'})
 res = res.drop(res.columns[len(res.columns)-2], axis = 1)
 print(res)
 
-df = pd.read_excel('csv/преступления_несовершеннолетние.xls', header=1)
+df = pd.read_excel('../data/raw/преступления_несовершеннолетние.xls', header=1)
 df[df.columns[0]] = df[df.columns[0]].str.strip()
 res = res.merge(df[[df.columns[0], 'Среднее']], how='left', left_on='region', right_on=df.columns[0]).rename(columns={'Среднее':'num_of_underaged_criminals'})
 res = res.drop(res.columns[len(res.columns)-2], axis = 1)
 print(res)
 
-df = pd.read_excel('csv/рейтинговый_балл_качества_жизни.xlsx', header=0)
+df = pd.read_excel('../data/raw/рейтинговый_балл_качества_жизни.xlsx', header=0)
 df[df.columns[0]] = df[df.columns[0]].str.strip()
 res = res.merge(df[[df.columns[0], 'среднее']], how='left', left_on='region', right_on=df.columns[0]).rename(columns={'среднее':'life_quality'})
 res = res.drop(res.columns[len(res.columns)-2], axis = 1)
 print(res)
 
-df = pd.read_excel('csv/численность_населения.xlsx')
+df = pd.read_excel('../data/raw/численность_населения.xlsx')
 df[df.columns[0]] = df[df.columns[0]].str.strip()
 res = res.merge(df[[df.columns[0], 'Среднее']], how='left', left_on='region', right_on=df.columns[0]).rename(columns={'Среднее':'population_size'})
 res = res.drop(res.columns[len(res.columns)-2], axis = 1)
 print(res)
 
-df = pd.read_excel('csv/Число_зарегистрированных_преступлений_в_России.xlsx', header=1)
+df = pd.read_excel('../data/raw/Число_зарегистрированных_преступлений_в_России.xlsx', header=1)
 df['mean'] = df.iloc[:, 1:].mean(axis=1)
 df[df.columns[0]] = df[df.columns[0]].str.strip()
 res = res.merge(df[[df.columns[0], 'mean']], how='left', left_on='region', right_on=df.columns[0]).rename(columns={'mean':'num_of_crimes'})
 res = res.drop(res.columns[len(res.columns)-2], axis = 1)
 print(res)
 
-res.to_csv('csv/dataset.csv', index=False)
+res.to_csv('../data/preprocessed/dataset.csv', index=False)
